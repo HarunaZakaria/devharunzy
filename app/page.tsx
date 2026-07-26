dfff22b07b84b8585d7771517969052b4b437c74
Harunzy <Harunzy55@gmail.com>
2026-07-26 03:58:44 -0700

    add nav and hero content

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,62 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="flex min-h-screen flex-col items-center justify-between p-12">
+      {/* Header */}
+      <div className="header flex  justify-between w-full">
+        <div className="logo">
+          <h1 className="text-2xl font-bold">Dev.Harunzy</h1>
+        </div>
+        <div className="nav">
+          <ul className="flex gap-16">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Skills</a>
+            </li>
+            <li>
+              <a href="/contact">Project</a>
+            </li>
+            <li>
+              <a href="/contact">Experience</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p>Let's talk</p>
+        </div>
+        {/* hero section */}
+      </div>
+      <div className="hero flex justify-between w-full mt-12">
+        <div className="hero-text w-1/2 flex flex-col justify-center">
+          <p className="text-xl  mb-4">Available for new opportunities</p>
+          <h1 className="text-4xl font-bold mb-4">Hi, I'm Harunzy</h1>
+          <p className="text-lg mb-4">
+            I am a passionate web developer with experience in building modern
+            web applications using React, Next.js, and other technologies. I
+            love creating user-friendly and responsive websites that provide a
+            great user experience.
           </p>
+          <div className="flex gap-4">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded">
+              View projects
+            </button>
+            <button className="bg-gray-200 text-gray-800 px-4 py-2 rounded">
+              Download CV / Resume
+            </button>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="hero-image  border-2 border-gray-300 rounded-lg overflow-hidden flex justify-center items-center">
+          <Image src="/haruna.jpeg" alt="Hero Image" width={500} height={300} />
         </div>
-      </main>
+      </div>
     </div>
   );
 }
