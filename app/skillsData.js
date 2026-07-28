82bc1fa7ff2323176fde1c4c3f55f81f1b5fb94b
Harunzy <Harunzy55@gmail.com>
2026-07-28 13:42:15 -0700

    add skillsData.tsx

---
+++ app/skillsData.js
@@ -0,0 +1,45 @@
+const skills = [
+  {
+    id: 1,
+    name: "Frontend Dev",
+    tools: [
+      { name: "React", level: "93%" },
+      { name: "Next.js", level: "80%" },
+      { name: "TypeScript", level: "87%" },
+      { name: "JavaScript", level: "97%" },
+      { name: "HTML5", level: "99%" },
+      { name: "CSS3", level: "99%" },
+      { name: "Tailwind CSS", level: "94%" },
+      { name: "Bootstrap", level: "96%" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Backend Dev",
+
+    tools: [
+      { name: "Node.js", level: "90%" },
+      { name: "Express", level: "80%" },
+      { name: "MongoDB", level: "85%" },
+      { name: "PostgreSQL", level: "80%" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Version Control",
+    tools: [
+      { name: "Git", level: "90%" },
+      { name: "Github", level: "99%" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Deployment",
+    tools: [
+      { name: "Netlify", level: "97%" },
+      { name: "Vercel", level: "95%" },
+    ],
+  },
+];
+
+export default skills;
