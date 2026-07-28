334415577179ff1e868df07f15bae189aadfc062
Harunzy <Harunzy55@gmail.com>
2026-07-28 15:15:41 -0700

    add

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import skillsData from "../skillsData";
+import Skill from "./Skill";
 
 const Skills = () => {
-  // skillsData.forEach((skillData) => {
-  //   console.log(skillData);
-  // });
   return (
     <div>
       <div className="flex mt-12 mb-24 flex-col">
@@ -16,47 +14,7 @@ const Skills = () => {
           </p>
         </div>
         <div>
-          <div className="flex flex-wrap w-full">
-            {skillsData.map((skillData) => (
-              <div
-                key={skillData.id}
-                className="m-4 p-4  bg-gray-200 text-gray-800 rounded "
-              >
-                <h3 className="font-bold">{skillData.name}</h3>
-                {skillData.tools.map((tool) => (
-                  <div>
-                    <p className="px-4 py-2">
-                      {tool.name} - {tool.level}
-                    </p>
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
-          {/* <div className="w-1/3 p-4  bg-gray-200 text-gray-800 rounded">
-            <h3>Backend</h3>
-            <p className=" px-4 py-2">Node.js</p>
-            <p className=" px-4 py-2">Express</p>
-            <p className=" px-4 py-2 ">MongoDB</p>
-            <p className=" px-4 py-2 ">PostgreSQL</p>
-          </div>
-          <div className="w-1/3 p-4  bg-gray-200 text-gray-800 rounded">
-            <h3>Styling</h3>
-            <p className="px-4 py-2 ">Tailwind CSS</p>
-            <p className="px-4 py-2 ">Bootstrap</p>
-            <p className="px-4 py-2 ">Material UI</p>
-          </div>
-          <div className="w-1/3 p-4  bg-gray-200 text-gray-800 rounded">
-            <h3>Version Control</h3>
-            <p className="px-4 py-2 ">Git</p>
-            <p className="px-4 py-2 ">GitHub</p>
-            <p className="px-4 py-2 ">GitLab</p>
-          </div>
-          <div className="w-1/3 p-4  bg-gray-200 text-gray-800 rounded">
-            <h3>Deployment</h3>
-            <p className="px-4 py-2 ">Vercel</p>
-            <p className="px-4 py-2 ">Netlify</p>
-          </div> */}
+          <Skill />
         </div>
       </div>
     </div>
