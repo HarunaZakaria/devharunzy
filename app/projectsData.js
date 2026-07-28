e8d1e9efb7ea7aa19cc4e634465b29a35dbb1084
Harunzy <Harunzy55@gmail.com>
2026-07-28 15:14:47 -0700

    add projectData.js

---
+++ app/projectsData.js
@@ -0,0 +1,27 @@
+const projectsData = [
+  {
+    id: Date.now() + 1,
+    title: "Project 1",
+    image: "/haruna.jpeg",
+    description: "Description of project 1",
+    technologies: ["React", "Next.js"],
+    links: ["GitHub", "Live"],
+  },
+  {
+    id: Date.now() + 2,
+    title: "Project 2",
+    image: "/haruna.jpeg",
+    description: "Description of project 2",
+    technologies: ["React", "Next.js"],
+    links: ["GitHub", "Live"],
+  },
+  {
+    id: Date.now() + 3,
+    title: "Project 3",
+    image: "/haruna.jpeg",
+    description: "Description of project 3",
+    technologies: ["React", "Next.js"],
+    links: ["GitHub", "Live"],
+  },
+];
+export default projectsData;
